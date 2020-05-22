16d7bb1cf9f3a560b665d3f30714684ea760e9de
moriyahschick <36717198+moriyahschick@users.noreply.github.com>
2020-05-22 03:23:56 -0400

    Cleaned up and began work on further metrics

--- senior_thesis.py
+++ senior_thesis.py
@@ -7,15 +7,11 @@ Created on Fri Apr 24 13:57:38 2020
 """
 import nltk
 from nltk.tree import Tree
-from nltk.corpus import treebank
 from nltk.corpus import ptb
-from typing import Tuple, List
 import os, random
 import pickle
-from nltk.classify import apply_features
-from nltk.classify.scikitlearn import SklearnClassifier
-from sklearn.naive_bayes import MultinomialNB
-#from sklearn.linear_model import LogisticRegression, SGDClassifier
+import nltk.metrics
+import collections
 
 
 def with_internal_labels(parsed_sents):
@@ -31,12 +27,9 @@ def with_internal_labels(parsed_sents):
             encoding += "1 "
             encoding += node.label()
             encoding += " "
-            #print("1", node.label(), end=" ")
         # if it is not a leaf node
         else:
             encoding += "0 "
-            #print("0", end=" ")
-    #print(encoding)
     return encoding
     
 def without_internal_labels(parsed_sents):
@@ -86,92 +79,8 @@ def get_subtrees(parsed_sent):
     for key in subtree_dict.keys():
         feature_dict[key] = subtree_dict[key]
 
-
-# below is from Professor Waxman
-def get_tree_structure(tree):
-    a = []
-    
-    def f(tree):
-        nonlocal a
-        
-        # first check if this is a leaf
-        # in wsj corpus, could either be (DT the)
-        # or even just "in" as the single element
-        if (len(tree) == 1 and isinstance(tree[0], str)) or \
-                                isinstance(tree, str):
-            a.append(0)
-            a.append(tree.label())  #adding branching factor
-            return
-        
-        # otherwise, add branching
-        # and preorder traverse thru children
-        a.append(len(tree))
-        for child in tree:
-            f(child)
-            
-    f(tree)
-    return a
-
-def get_tree_structure_and_labels(tree) -> Tuple[List[int], List[str]]:
-    a = get_tree_structure(tree)
-    labels = [x for x in a if isinstance(x, str)]
-    structure = [x for x in a if isinstance(x, int)]
-    return structure, labels
-
-
-def print_subtree_structures(tree):
-    # this is a demo of how we can get all
-    # subtree structures to train on. you would
-    # write similar code. Subtrees would first give
-    # you the full tree, then the left subtree,
-    # then the right subtree, etc. until we get to
-    # the subtrees which are leaves
-    x = list(tree.subtrees())
-    for st in x:
-        a = get_tree_structure(st)
-        print(a)
-    return x
-
 def get_height(t):
     return t.height()
-
-def get_tilt(tree):
-    # we are defining the "tilt" of a tree
-    # as how often the tree gets extended
-    # towards the right (most often, in most
-    # trees) and how often towards the left.
-    # For an n-ary tree, not so helpful, but good
-    # for binary trees
-    
-    # need to double check for wsj trees with period
-    # at the end whether that is the issue
-    
-    left = 0
-    right = 0
-    
-    # so long as we have not reached a leaf
-    while not (len(tree) == 1 and isinstance(tree[0], str)):
-        if len(tree) == 1:
-            # handle unary branching
-            tree = tree[0]
-        else:
-            # look through subtrees, find one with max depth
-            maxpos = -1
-            maxheight = -1
-            for i, node in enumerate(tree):
-                if node.height() > maxheight:
-                    maxheight = node.height()
-                    maxpos = i
-                    
-            if maxpos < len(tree) // 2:
-                left += 1
-            else:
-                right += 1
-                
-        # follow the deeper path
-        tree = tree[maxpos]
-        
-    return left, right
 
 # get genre of each file
 print("Getting genre for each file...")
@@ -203,7 +112,7 @@ except Exception as e:
 if len(feature_set) != 0:     # aka was previously created and then unpickled
     pass
 else:                         # does not exist and must be created
-    # add WSJ corpus to feature set -- totally works
+    # add WSJ corpus to feature set
     # loop through each directory
     for i in range(0, 25):
         
@@ -248,17 +157,10 @@ else:                         # does not exist and must be created
                             height = get_height(parsed_sent)
                             get_productions(parsed_sent) # by running will add them to feature_dict
                             get_subtrees(parsed_sent) #same as get_productions()
-                            #length = get_sentence_length(parsed_sent)
-                            #tilt = get_tilt(parsed_sent)
-                        
                         
                             feature_dict['without_internal_labels'] = no_internal_labels
                             feature_dict['height'] = height
-                            #feature_dict['tilt'] = (left, right)
                         
-                            #yes_internal_labels = with_internal_labels(parsed_sent)
-                        
-                            #feature_set += [({'without_internal_labels': no_internal_labels, 'height': height, 'productions': productions, 'length': length}, genre)]
                             feature_set += [(feature_dict, genre)]
                         if genreCount[genre] >= NUM_EXAMPLES:
                             break
@@ -294,13 +196,11 @@ else:                         # does not exist and must be created
         for file in needed_files_in_dir:
                 
             file_name = 'BROWN/' + dir_name + '/' + file
-            #print(file_path)
     
             try:
                 num_sentences = len(ptb.parsed_sents(file_name))
                 genre = genre_dict[file_name]
                 genre_dict[file_name] = genre
-                #print(file_name, genre)
             
             except:
                 print("This file does not exist and a genre cannot be found for it")
@@ -321,14 +221,10 @@ else:                         # does not exist and must be created
                             height = get_height(parsed_sent)
                             get_productions(parsed_sent)
                             get_subtrees(parsed_sent) #same as get_productions()
-                            #left, right = get_tilt(parsed_sent)
                             
                             feature_dict['without_internal_labels'] = no_internal_labels
                             feature_dict['height'] = height
-                            #feature_dict['tilt'] = (left, right)
-                            
-                            #yes_internal_labels = with_internal_labels(parsed_sent)
-                            
+
                             feature_set += [(feature_dict, genre)]
                             
                         if genreCount[genre] >= NUM_EXAMPLES:
@@ -418,31 +314,43 @@ for genre in d_testing:
         if genre == 'romance':
             romance_testing.append(feature)  
 
+try:
+    # if we have the pickled trained classifiers, open and deserialize them
+    pickled_naiveBayes_classifier = open('pickled_naiveBayes_classifier', 'rb')
+    naiveBayes_classifer = pickle.load(pickled_feature_set)
+    pickled_naiveBayes_classifier.close()
+    
+    pickled_maxEnt_classifier = open('pickled_maxEnt_classifier', 'rb')
+    maxEnt_classifer = pickle.load(pickled_maxEnt_classifier)
+    pickled_maxEnt_classifier.close()
+    
+except:
+    # if we didn't have the pickled trained classifiers, train and serialize them
+    
+    naiveBayes_classifier = nltk.NaiveBayesClassifier.train(training)
+    maxEnt_classifier = nltk.MaxentClassifier.train(training)
+    
+    pickled_naiveBayes_classifier = open('pickled_naiveBayes_classifier', 'wb')
+    pickle.dump(naiveBayes_classifier, pickled_naiveBayes_classifier)
+    pickled_naiveBayes_classifier.close()
+
+    pickled_maxEnt_classifier = open('pickled_maxEnt_classifier', 'wb')
+    pickle.dump(maxEnt_classifier, pickled_maxEnt_classifier)
+    pickled_maxEnt_classifier.close()
+
 print("Beginning classification...")
-naiveBayes_classifier = nltk.NaiveBayesClassifier.train(training)
-maxEnt_classifier = nltk.MaxentClassifier.train(training)
-
-pickled_naiveBayes_classifier = open('pickled_naiveBayes_classifier', 'wb')
-pickle.dump(naiveBayes_classifier, pickled_naiveBayes_classifier)
-pickled_naiveBayes_classifier.close()
-
-pickled_maxEnt_classifier = open('pickled_maxEnt_classifier', 'wb')
-pickle.dump(maxEnt_classifier, pickled_maxEnt_classifier)
-pickled_maxEnt_classifier.close()
-
 sorted(naiveBayes_classifier.labels())      # could have used either classifier since both have the same labels
 
-#naiveBayes_classifier.prob_classify_many(practice_set)
 
-humor_pdist           = naiveBayes_classifier.prob_classify_many(humor_testing)
-news_pdist            = naiveBayes_classifier.prob_classify_many(news_testing)
-lore_pdist            = naiveBayes_classifier.prob_classify_many(lore_testing)
-belles_lettres_pdist  = naiveBayes_classifier.prob_classify_many(belles_lettres_testing)
-fiction_pdist         = naiveBayes_classifier.prob_classify_many(fiction_testing)
-mystery_pdist         = naiveBayes_classifier.prob_classify_many(mystery_testing)
-science_fiction_pdist = naiveBayes_classifier.prob_classify_many(science_fiction_testing)
-adventure_pdist       = naiveBayes_classifier.prob_classify_many(adventure_testing)
-romance_pdist         = naiveBayes_classifier.prob_classify_many(romance_testing)
+humor_pdist           = maxEnt_classifier.prob_classify_many(humor_testing)
+news_pdist            = maxEnt_classifier.prob_classify_many(news_testing)
+lore_pdist            = maxEnt_classifier.prob_classify_many(lore_testing)
+belles_lettres_pdist  = maxEnt_classifier.prob_classify_many(belles_lettres_testing)
+fiction_pdist         = maxEnt_classifier.prob_classify_many(fiction_testing)
+mystery_pdist         = maxEnt_classifier.prob_classify_many(mystery_testing)
+science_fiction_pdist = maxEnt_classifier.prob_classify_many(science_fiction_testing)
+adventure_pdist       = maxEnt_classifier.prob_classify_many(adventure_testing)
+romance_pdist         = maxEnt_classifier.prob_classify_many(romance_testing)
 
 pdist_arr = [(humor_pdist, 'humor'), 
              (news_pdist, 'news'), 
@@ -455,6 +363,7 @@ pdist_arr = [(humor_pdist, 'humor'),
              (romance_pdist, 'romance')]
 
 for genre_pdist, genre in pdist_arr:
+    print(genre)
     for i in range(0, len(genre_pdist), 5):
         # take 5 consecutive sentences and take product of their probabilities for each genre and
         # see if they match the assumed one (here its humor)
@@ -494,28 +403,32 @@ for genre_pdist, genre in pdist_arr:
             print(actual_genre_prob, ": ", all_probs[actual_genre_prob], max_prob, ": ", all_probs[max_prob])
         except Exception as e:
             print(e)
-
-naiveBayes_classifier.classify_many(practice_set)
-for pdist in naiveBayes_classifier.prob_classify_many(practice_set):
-    print('%.4f %.4f %.4f %.4f %.4f %.4f %.4f %.4f %.4f' % (pdist.prob('humor'), 
-                                                           pdist.prob('news'), 
-                                                           pdist.prob('lore'), 
-                                                           pdist.prob('belles_lettres'), 
-                                                           pdist.prob('fiction'), 
-                                                           pdist.prob('mystery'), 
-                                                           pdist.prob('science_fiction'), 
-                                                           pdist.prob('adventure'), 
-                                                           pdist.prob('romance')))
+            pass
 
 print("Naive Bayes accuracy: ", nltk.classify.accuracy(naiveBayes_classifier, testing))
 naiveBayes_classifier.show_most_informative_features(20)
 
-print("Maxent accuracy: ", nltk.classify.accuracy(maxEnt_classifier, test_set))
+print("Maxent accuracy: ", nltk.classify.accuracy(maxEnt_classifier, testing))
 maxEnt_classifier.show_most_informative_features(20)
 
-MNB_clf = SklearnClassifier(MultinomialNB())
-MNB_clf.train(train_set)
-print("MultinomialNB accuracy percent:",nltk.classify.accuracy(MNB_clf, test_set))
+
+# metrics not working
+refsets = collections.defaultdict(set)
+testsets = collections.defaultdict(set)
+
+for i, (feats, label) in enumerate(testing):
+    refsets[label].add(i)
+    observed = naiveBayes_classifier.classify(feats)
+    testsets[observed].add(i)
+
+ 
+print('pos precision:', nltk.metrics.precision(refsets['pos'], testsets['pos']))
+print('pos recall:', nltk.metrics.recall(refsets['pos'], testsets['pos']))
+print('pos F-measure:', nltk.metrics.f_measure(refsets['pos'], testsets['pos']))
+print('neg precision:', nltk.metrics.precision(refsets['neg'], testsets['neg']))
+print('neg recall:', nltk.metrics.recall(refsets['neg'], testsets['neg']))
+print('neg F-measure:', nltk.metrics.f_measure(refsets['neg'], testsets['neg']))
+
 
 
 """
